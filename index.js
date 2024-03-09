c212037b3c1b969c3064670cb83bd0610b7d64ae
belajar-vue-2 <aryaadi229@gmail.com>
2024-03-09 20:23:18 +0700

    feat:register page

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const theaterRouter = require('./routes/theater');
 const adminRouter = require('./routes/admin');
@@ -9,17 +10,50 @@ const adminRouter = require('./routes/admin');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cookieParser('secret-key'));
+const secret = 'secret-key';
+app.use(cookieParser(secret));
 app.use('/theater', theaterRouter);
 app.use('/admin', adminRouter);
+app.use(session({
+    secret: secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false
+    }
+}))
 
 app.get('/sign', (req, res) => {
-    res.cookie('tas', 'ransel', {signed: true});
+    res.cookie('tas', 'ransel', {
+        signed: true
+    });
     console.log(req.signedCookies);
-    const {tas} = req.signedCookies;
+    const {
+        tas
+    } = req.signedCookies;
     res.send(`Sign Cookies, ${tas}`);
 })
 
+app.get('/count', (req, res) => {
+    if (req.session.count) {
+        req.session.count++;
+    } else {
+        req.session.count = 1;
+    }
+    res.send(`Count ${req.session.count}`);
+})
+
+app.get('/register', (req, res) => {
+    const {
+        name = 'Anonym'
+    } = req.query;
+    req.session.username = name;
+    res.redirect('/dashboard');
+})
+
+app.get('/dashboard', (req, res) => {
+    res.send(`Welcome back ${req.session.username}`);
+})
 
 app.listen(8080, () => {
     console.log('listening on port http://localhost:8080');
