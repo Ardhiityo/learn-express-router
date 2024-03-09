3beb62be95f56a5272249cee97c4e1f6597fbd44
belajar-vue-2 <aryaadi229@gmail.com>
2024-03-09 16:00:41 +0700

    feat:signed cookies

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const cookies = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 
 const theaterRouter = require('./routes/theater');
 const adminRouter = require('./routes/admin');
@@ -9,11 +9,16 @@ const adminRouter = require('./routes/admin');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cookies());
+app.use(cookieParser('secret-key'));
 app.use('/theater', theaterRouter);
 app.use('/admin', adminRouter);
 
-
+app.get('/sign', (req, res) => {
+    res.cookie('tas', 'ransel', {signed: true});
+    console.log(req.signedCookies);
+    const {tas} = req.signedCookies;
+    res.send(`Sign Cookies, ${tas}`);
+})
 
 
 app.listen(8080, () => {
--- routes/admin.js
+++ routes/admin.js
@@ -4,12 +4,17 @@ const router = express.Router();
 router.use((req, res, next) => {
     if (req.query.isAdmin) {
         next();
+    } else {
+        res.send('you\'re not admin');
     }
-    res.send('you\'re not admin');
 })
 
 router.get('/', (req, res, next) => {
-    res.send('You\'re Admin');
+    const {
+        admin = '123'
+    } = req.cookies;
+    res.cookie('admin', '123osdnvsd');
+    res.send(`You\'re Admin, ${admin}`);
 })
 
 module.exports = router;
